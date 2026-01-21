d498448f42b9758ca26d8e69afa283babb462a03
antor94 <antorremeni@gmail.com>
2026-01-21 12:14:41 +0600

    change

--- src/Components/Home/AllProduct.jsx
+++ src/Components/Home/AllProduct.jsx
@@ -1,25 +1,67 @@
-import React from 'react'
-import CommonHead from '../Common/CommonHead'
+import React from "react";
+import CommonHead from "../Common/CommonHead";
+import ProductCard from "./product/ProductCard";
+import productImg from "../../assets/productImg.png";
 
 const AllProduct = () => {
   return (
     <>
-    
-    
-<div>
-    <div>
-    <CommonHead h2={'Today’s'} hp={'Flash Sales'} />
+      <div className=" pt-[154px] pb-[60px]">
+        <div>
+          <CommonHead h2={"Today’s"} hp={"Flash Sales"} />
+        </div>
 
-    </div>
-</div>
-    
-    
-    
-    
-    
-    
+        <div className="grid grid-cols-4 gap-[30px]">
+          <ProductCard
+            image={productImg}
+            title="AK-900 Wired Keyboard"
+            price="960"
+            oldPrice="1160"
+            discount="35"
+            rating={4}
+            reviews="75"
+          />
+          <ProductCard
+         image={productImg}
+            title="AK-900 Wired Keyboard"
+            price="960"
+            oldPrice="1160"
+            discount="35"
+            rating={4}
+            reviews="75"
+          />
+          <ProductCard
+  image={productImg}
+            title="AK-900 Wired Keyboard"
+            price="960"
+            oldPrice="1160"
+            discount="35"
+            rating={4}
+            reviews="75"
+          />
+          <ProductCard
+           image={productImg}
+            title="AK-900 Wired Keyboard"
+            price="960"
+            oldPrice="1160"
+            discount="35"
+            rating={4}
+            reviews="75"
+          />
+         
+         </div>
+         <div className="text-center">
+
+          <button
+            type="button"
+            className="w-[234px] bg-[#DB4444] text-[16px]  mt-[40px] mb-[16px] font-medium font-poppins text-white py-[16px] rounded-md hover:bg-red-600 transition"
+            >
+            View All Products
+          </button>
+                </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default AllProduct
+export default AllProduct;
