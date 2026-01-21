a8e84b22bc1e975f1e278b016df8238780d653a8
antor94 <antorremeni@gmail.com>
2026-01-21 11:31:02 +0600

    change

--- src/Components/Home/NewArrival.jsx
+++ src/Components/Home/NewArrival.jsx
@@ -1,30 +1,31 @@
 import { FaArrowRight } from "react-icons/fa";
 
-import arivalImg from '../../assets/arivalImg1.png'
-import arivalImg2 from '../../assets/arivalImg2.png'
-import arivalImg3 from '../../assets/arivalImg3.png'
-import arivalImg4 from '../../assets/arivalImg4.png'
+import arivalImg from "../../assets/arivalImg1.png";
+import arivalImg2 from "../../assets/arivalImg2.png";
+import arivalImg3 from "../../assets/arivalImg3.png";
+import arivalImg4 from "../../assets/arivalImg4.png";
+import Breadcrumb from "../Common/Breadcrumb";
+import CommonHead from "../Common/CommonHead";
 
 const NewArrival = () => {
   return (
-
     <>
-    
-    <div>
-        <div><img src={arivalImg} alt="images" /></div>
-        <div>
+      <div className="mt-[168px]">
+        <CommonHead h2={"Featured"} hp={"New Arrival"} />
+        <div className="grid grid-cols-2 gap-[30px] mt-[60px]">
+          <div>
+            <img src={arivalImg} alt="images" />
+          </div>
+          <div>
             <img src={arivalImg2} alt="images" />
-            <div>
-                <img src={arivalImg3} alt="images" />
-                <img src={arivalImg4} alt="imges" />
+            <div className="grid grid-cols-2 gap-[30px] mt-[32px]">
+              <img src={arivalImg3} alt="images" />
+              <img src={arivalImg4} alt="imges" />
             </div>
+          </div>
         </div>
-    </div>
-    
-    
+      </div>
     </>
-
- 
   );
 };
 
